1a7d484daf7d0b33919a1cb5ffa555ff3b836ad6
LeoMaia <leonardoz5z@hotmail.com>
2019-09-21 09:22:08 -0300

    Fix log service

--- api-email/src/services/logger.js
+++ api-email/src/services/logger.js
@@ -1,12 +1,13 @@
 
 const winston = require('winston')
+const colorizer = winston.format.colorize();
 const logger = winston.createLogger({
     format: winston.format.combine(
         winston.format.timestamp({ format: 'DD-MM-YY HH:mm:ss' }),
         winston.format.simple(),
-        winston.format.printf(msg => colorizer.colorize(msg.level, `${msg.timestamp} - ${msg.level}: `) + `${msg.message}`)
+        winston.format.printf(msg =>  colorizer.colorize(msg.level, `${msg.timestamp} - ${msg.level}: `) + `${msg.message}`)
     ),
-    transports: [new transports.Console()]
+    transports: [new winston.transports.Console()]
 
 })
 
